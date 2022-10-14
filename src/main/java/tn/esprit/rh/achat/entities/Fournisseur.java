50adaea7609339a4cb383ad2ae792c5be151e1fc
Safa HALLEK <safa.hallek@esprit.tn>
2022-10-14 20:38:48 +0100

    Test RetriveAllFournisseurs

--- src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Fournisseur.java
@@ -43,7 +43,13 @@ public class Fournisseur implements Serializable {
     private Set<SecteurActivite> secteurActivites;
     @OneToOne(cascade= CascadeType.ALL,fetch=FetchType.EAGER)
     private DetailFournisseur detailFournisseur;
-    
 
-	
+
+	public Fournisseur(long i, String code_safa, String libelle_safa, CategorieFournisseur ordinaire) {
+		this.idFournisseur = i;
+		this.code = code_safa;
+		this.libelle = libelle_safa;
+		this.categorieFournisseur = ordinaire;
+
+	}
 }
--- src/test/java/tn/esprit/rh/achat/FournisseurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurServiceTest.java
@@ -1,14 +1,23 @@
 package tn.esprit.rh.achat;
 
-import org.apache.el.stream.Stream;
+
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.runner.RunWith;
+import tn.esprit.rh.achat.entities.DetailFournisseur;
 import tn.esprit.rh.achat.entities.Fournisseur;
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
 import tn.esprit.rh.achat.services.FournisseurServiceImpl;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static tn.esprit.rh.achat.entities.CategorieFournisseur.ORDINAIRE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,11 +28,14 @@ public class FournisseurServiceTest {
 private FournisseurServiceImpl service;
 @MockBean
 private FournisseurRepository repository;
+DetailFournisseur df = new DetailFournisseur();
 
+@Test
 public void addFournisseurTest(){
-    when(repository.findAll()).thenReturn(Stream.
-            of(new Fournisseur(12, "code safa", "libelle safa"), new))
-
+    System.out.println("hhhhhh");
+    when(repository.findAll()).thenReturn(Stream
+            .of(new Fournisseur(12, "code safa", "libelle safa",ORDINAIRE), new Fournisseur(22, "Code HLK","Libelle HLK",ORDINAIRE)).collect(Collectors.toList()));
+    assertEquals(2, service.retrieveAllFournisseurs().size());
 }
 
 
