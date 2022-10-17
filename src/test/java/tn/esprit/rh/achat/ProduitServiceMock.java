da95106345014f1ec30cf3bd9a96c9d6eb83e705
louhaichi <ahmed.louhaichi@esprit.tn>
2022-10-17 10:21:58 +0100

    fix bug

--- src/test/java/tn/esprit/rh/achat/ProduitServiceMock.java
+++ src/test/java/tn/esprit/rh/achat/ProduitServiceMock.java
@@ -16,10 +16,13 @@ import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.repositories.ProduitRepository;
 import tn.esprit.rh.achat.services.ProduitServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -34,13 +37,46 @@ public class ProduitServiceMock {
     ProduitServiceImpl produitService;
 
     Produit p1 = new Produit(55L, "2365","produit1",50);
+    Produit p2 = new Produit(66L, "5681","produit5",120);
 
+
+    List<Produit> listProduits = new ArrayList<Produit>() {
+        {
+            add(p1);
+            add(new Produit(90L, "9687","produit2",30));
+            add(new Produit(46L, "4503","produit3",70));
+        }
+    };
     @Test
-    public void testRetrievProduit() {
+    public void testRetrieveProduit() {
         Mockito.when(produitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(p1));
         Produit produit1 = produitService.retrieveProduit(55L);
         assertNotNull(produit1);
-        //assertEquals(produit1.getIdProduit(),55L);
-
+        assertEquals(produit1.getIdProduit(),55L);
     }
+
+    @Test
+    public void testretrieveAllProduits() {
+        Mockito.when(produitRepository.findAll()).thenReturn(listProduits);
+        List<Produit> listproduit3 = produitService.retrieveAllProduits();
+        assertEquals(3, listproduit3.size());
+        //assertEquals(produit1.getIdProduit(),55L);
+    }
+
+    @Test
+    public void testaddProduit(){
+        Mockito.when(produitRepository.save(p1)).thenReturn(p1);
+        Produit produit1 = produitService.addProduit(p1);
+        assertNotNull(produit1);
+        Mockito.verify(produitRepository, times(1)).save(Mockito.any(Produit.class));
+    }
+
+
+    @Test
+    public void testdeleteProduit(){
+
+        produitService.deleteProduit(66L);
+        Mockito.verify(produitRepository, times(0)).delete(p2);
+    }
+
 }
