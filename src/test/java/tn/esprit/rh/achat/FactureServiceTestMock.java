01f48d39b41c9f19667e426afd29ccc69855a8b0
hiba-khalfaoui <hiba.khalfaoui@esprit.tn>
2022-10-18 16:19:21 +0100

    testtt

--- src/test/java/tn/esprit/rh/achat/FactureServiceTestMock.java
+++ src/test/java/tn/esprit/rh/achat/FactureServiceTestMock.java
@@ -1,16 +1,19 @@
 package tn.esprit.rh.achat;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assert.assertEquals;
+//import org.junit.Test;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
@@ -25,6 +28,7 @@ import tn.esprit.rh.achat.services.FactureServiceImpl;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
+
 public class FactureServiceTestMock {
 	@Mock
 	FactureRepository sr;
