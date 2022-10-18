33464a11d23ae8e7589eb4de634eccbdb3cf3af8
Safa HALLEK <safa.hallek@esprit.tn>
2022-10-18 23:23:28 +0100

    WebHook

--- src/test/java/tn/esprit/rh/achat/FournisseurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurServiceTest.java
@@ -35,7 +35,7 @@ public class FournisseurServiceTest {
 
     Fournisseur f1 = new Fournisseur(12L, "Code Safa","Libelle HLK",ORDINAIRE);
     Fournisseur f2 = new Fournisseur(22L, "Code Safa2","Libelle HLK2",ORDINAIRE);
-//Hiiiiiiiiiiiiiiii222
+//Hiiiiiiiiiiiiiiii222333333333333
     List<Fournisseur> listFournisseurs = new ArrayList<Fournisseur>() {
         {
             add(f1);
