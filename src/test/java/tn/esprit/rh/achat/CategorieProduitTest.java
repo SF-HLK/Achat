fa2c348cbca565978af0a8c596ff9330ff70c182
rayenhedfi <rayen.hedfi@esprit.tn>
2022-10-19 11:41:28 +0200

    test22

--- src/test/java/tn/esprit/rh/achat/CategorieProduitTest.java
+++ src/test/java/tn/esprit/rh/achat/CategorieProduitTest.java
@@ -4,6 +4,7 @@ package tn.esprit.rh.achat;
 import static org.mockito.Mockito.verify;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 import static org.mockito.BDDMockito.*;
@@ -21,8 +23,10 @@ import static org.mockito.Mockito.times;
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
 import tn.esprit.rh.achat.services.CategorieProduitServiceImpl;
+
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class CategorieProduitTest {
 
 @Mock	
@@ -32,14 +36,12 @@ CategorieProduitServiceImpl categorieProduit;
 
 CategorieProduit c1 = new CategorieProduit (1L,"abc","homme",null);
 
-
 @Test
 public void testRetrievecategorie() {
 Mockito.when(categorieProduitRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(c1));
 CategorieProduit categorie1 = categorieProduit.retrieveCategorieProduit(1L);
 Assertions.assertNotNull(categorie1);
 }
-
 
 @Test
 public void createcategorieproduitTest()
