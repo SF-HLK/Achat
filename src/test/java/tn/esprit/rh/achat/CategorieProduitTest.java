9a26795f0a4515057c1adbcb1fef0fca35fdff93
rayenhedfi <rayen.hedfi@esprit.tn>
2022-10-19 11:12:11 +0200

    test17

--- src/test/java/tn/esprit/rh/achat/CategorieProduitTest.java
+++ src/test/java/tn/esprit/rh/achat/CategorieProduitTest.java
@@ -2,33 +2,33 @@ package tn.esprit.rh.achat;
 
 
 import static org.mockito.Mockito.verify;
+
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
+
+
+
+
 import static org.mockito.BDDMockito.*;
-
-import static org.mockito.Mockito.times;
-
 import tn.esprit.rh.achat.entities.CategorieProduit;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
 import tn.esprit.rh.achat.services.CategorieProduitServiceImpl;
 
+
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
 public class CategorieProduitTest {
 
 @Mock	
@@ -37,10 +37,6 @@ CategorieProduitRepository categorieProduitRepository;
 CategorieProduitServiceImpl categorieProduit;
 
 CategorieProduit c1 = new CategorieProduit (1L,"abc","homme",null);
-
-
-
-
 
 
 @Test
